8663532729a0deed75b125c01a9699c1e8d56a29
vishalbhoyar24 <124866977+vishalbhoyar24@users.noreply.github.com>
2023-06-20 17:30:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let body = document.body;
+let line = document.querySelector("#line");
+line.style.animation = "spin 2s linear infinite";
+
+let style = document.createElement("style");
+style.innerHTML = `@keyframes spin{
+    from{
+        transform : rotate(0deg);
+    }
+    to{
+        transform : rotate(360deg);
+    }
+}`
+body.append(style);
